000060eebd709cd3b3bddc3ca136b5636cb4979a
alextitonis <alexandrostitonis@gmail.com>
2022-08-31 18:41:31 +0300

    repo cleanup

--- Assets/Scripts/ScreenManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class ScreenManager : MonoBehaviour
-{
-    public abstract void OnUpdate(bool enable);
-}
